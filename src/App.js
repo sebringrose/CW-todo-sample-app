0c37cc9058450859d14a9996acc95b346f658d30
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-02-26 16:01:54 +0100

    upgrade to v1.0.1

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import { increment } from './actions';
 class App extends Component {
 
   render () {
+
     return (
       <div>
         <h1>Counter : {this.props.counter}</h1>
         <button
 
         >+</button>
-        <button
+        <button 
 
-        >-</button>
+        > - </button>
+        <hr/>
+        { 
+          // Implement the todo list
+
+        }
       </div>
     );
   }
@@ -23,6 +29,7 @@ const mapStateToProps = (state) => ({
   // Map your state to props
 
 });
+
 const mapDispatchToProps = (dispatch) => ({
   // Map your dispatch actions
   increment: () => dispatch(increment()),
