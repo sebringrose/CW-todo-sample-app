7c64f0b10cc296139af61cedb93a89ac22a8f34d
Sebastien Ringrose <seb@codeworks.me>
2022-10-20 13:15:28 +0100

    feat: mongo atlas setup

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,14 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import conf from '../config.js';
 
-const client = new MongoClient(conf.dbUrl);
+const uri = "mongodb+srv://admin:admin123@cw-todo-sample-app.tikr14v.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// const client = new MongoClient(conf.dbUrl, { 
+//   useNewUrlParser: true, 
+//   useUnifiedTopology: true, 
+//   serverApi: ServerApiVersion.v1 
+// });
 
 async function run() {
   try {
