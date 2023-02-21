113ca2e1a20948672337e70fbfb056401d6bb0d0
Sebastien Ringrose <seb@codeworks.me>
2023-02-21 19:49:43 +0000

    fix: ci db

--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,14 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import conf from '../config.js';
 
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const uri = "mongodb://localhost:27017";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// const client = new MongoClient(conf.dbUrl, { 
-//   useNewUrlParser: true, 
-//   useUnifiedTopology: true, 
-//   serverApi: ServerApiVersion.v1 
-// });
+const client = new MongoClient(conf.dbUrl, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  serverApi: ServerApiVersion.v1 
+});
 
 async function run() {
   try {
